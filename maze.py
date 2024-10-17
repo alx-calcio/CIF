d9e590e34f4749a2115f58aafb4087db58bde4a5
alx-calcio <117914199+alx-calcio@users.noreply.github.com>
2024-10-17 11:34:31 +0000

    Adding Maze class

--- maze.py
+++ maze.py
@@ -5,8 +5,34 @@ class Node:
         self.action = action
 
 
-class StackFrontier:
+class QueueFrontier:
     def __init__(self):
         self.frontier = []
+
     def add(self, node):
         self.frontier.append(node)
+    
+    def empty(self):
+        return len(self.frontier) == 0
+    
+    def remove(self):
+        if self.empty():
+            print("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier.remove(node)
+            return node
+        
+class Maze:
+    def __init__(self, filename):
+        with open(filename) as f:
+            contents = f.read()
+        if contents.count("A") != 1:
+            raise Exception("A")
+        if contents.count("B") != 1:
+            raise Exception("B")
+        
+        contents = contents.splitlines()
+        self.height = len(contents)
+        self.width = max(len(line) for line in contents)
+    
